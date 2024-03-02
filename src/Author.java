5e24574a49e2cb410d8959d6fc7d1a24956da1fe
user <Seregawinner92@gmail.com>
2024-03-02 14:13:05 +0300

    homework class and object lesson two

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private final String name;
 
@@ -16,4 +18,26 @@ public class Author {
         this.familiya = familiya;
     }
 
+    @Override
+    public String toString() {
+        return name + familiya;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(getName(), author.getName()) && Objects.equals(getFamiliya(), author.getFamiliya());
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getFamiliya());
+    }
 }
